15eaab9b6b8b45b6ec9846bd8fe5b06498efe3cc
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2017-10-27 15:12:57 +0300

    rebuild

--- src/components/MenuView/MenuViewDefault.js
+++ src/components/MenuView/MenuViewDefault.js
@@ -4,13 +4,18 @@ import React, { Component } from 'react';
 class MenuViewDefault extends Component {
   constructor(props) {
     super(props);
+    this.state = { option: props.option };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ option: nextProps.option });
   }
 
   render() {
     return (
       <div className="catalogue-wrapper">
         <div className="catalogue-inner-title">
-          <span className="nav-inner-title">{this.props.option}</span>
+          <span className="nav-inner-title">{this.state.option}</span>
         </div>
       </div>
     );
